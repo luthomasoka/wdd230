6a9e76e64692e7177877d3b860495f29179999c2
Lutho Masoka <luthomasoka@gmail.com>
2022-05-21 23:41:59 +0200

    W05

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -1,3 +1,15 @@
+const displayBannerMsg = () => {
+    let currentDate = new Date().getDay();
+    let msg = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+
+    if (currentDate == 1 || currentDate == 2) {
+        document.querySelector('.banner-msg p').innerHTML = msg;
+    } else {
+        document.querySelector('.banner-msg p').style.display = "none";
+    }
+}
+
+const weekday = displayBannerMsg();
 const todaysDate = new Date(document.lastModified);
 const dateField = document.querySelector('#dateTime');
 const date = new Date();
@@ -16,3 +28,5 @@ document.getElementById("currentdate").innerHTML = todaysDate.toLocaleString('en
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+displayBannerMsg();
