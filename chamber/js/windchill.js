59d6454e92ca3e1949dfa2bba88e8563d7138248
Lutho Masoka <luthomasoka@gmail.com>
2022-05-28 21:18:56 +0200

    W06

---
+++ chamber/js/windchill.js
@@ -0,0 +1,20 @@
+const speed = document.querySelector('#speed').textContent;
+const temp = document.querySelector('.temp').textContent;
+let windChill = 0;
+
+// console.log(speed);
+// console.log(temp);
+
+const celsToFahr = temp * 9/5 + 32;
+const kmphToMph = speed * 0.6214;
+
+if (celsToFahr <= 50 && kmphToMph > 3.0) {
+    windChill = 35.74 + 0.6215 * celsToFahr - 35.75 * Math.pow(kmphToMph, 0.16) + 0.4275 * celsToFahr * Math.pow(kmphToMph, 0.16);
+    //Conversion to Celsius
+    windChill = (windChill - 32) * 5/9;
+    document.querySelector('#chill').innerHTML = `${windChill.toFixed(2)} km/h`;
+
+} else { 
+    windChill = 'N/A';
+    document.querySelector('#chill').innerHTML = windChill;
+}
